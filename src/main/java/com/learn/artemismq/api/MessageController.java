e9afb6be55f153330170183e1eee9efb82b63612
yossi78 <yossi78@gmail.com>
2021-12-05 17:22:26 +0200

    add2

--- src/main/java/com/learn/artemismq/api/MessageController.java
+++ src/main/java/com/learn/artemismq/api/MessageController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-// test
 @RestController
 @Slf4j
 @RequestMapping(value = "/artemis")
